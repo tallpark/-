6408aebba451af5cd5930f772f893a04d9985898
tallpark <tallspark@naver.com>
2022-12-16 16:39:15 +0900

    수정..

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const post_writeRouter = require('./routes/post.js');
+const postRouter = require('./routes/post.js');
 const connect = require("./schemas");
 connect();
 
 app.use(express.json());
-app.use("/api", post_writeRouter);
+app.use("/api", postRouter);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
